2a8a883772d9b47f79ed5a5db32787075d86051c
Nirav Lathiya <83869023+Nirav591@users.noreply.github.com>
2024-05-22 17:50:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ var app = express();
 app.get('/', function (req, res) {
   res.send('Hello Nirav!');
 });
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(6340, function () {
+  console.log('Example app listening on port 6340!');
 });
