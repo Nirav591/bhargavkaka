afe4c3e0be8f82285a0884bc7139746699192d5a
Nirav591 <nirav.v.lathiya@gmail.com>
2024-05-24 22:02:12 -0700

    added

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const userRoutes = require('./routes/userRoutes');
+const cors = require('cors');
+
 
 const app = express();
 const port = 6350;
 
 app.use(bodyParser.json());
+app.use(cors({origin:"https://bhargavkaka.libertyworld.in/"}));
 app.use('/api/user', userRoutes);
 
 app.listen(port, () => {
