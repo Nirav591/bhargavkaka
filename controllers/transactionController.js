cd9242caaa5965407952b52571bdc026ee147123
Nirav591 <nirav.v.lathiya@gmail.com>
2024-05-25 19:19:54 +0530

    added

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -21,11 +21,35 @@ exports.addTransaction = (req, res) => {
 exports.getCustomerTransactions = (req, res) => {
   const { id } = req.params;
 
-  db.query('SELECT * FROM transactions WHERE customer_id = ?', [id], (err, result) => {
+  db.query('SELECT * FROM transactions WHERE customer_id = ?', [id], (err, transactions) => {
     if (err) {
       res.status(500).json({ message: 'Error occurred while fetching transactions' });
     } else {
-      res.status(200).json({ transactions: result });
+      db.query(
+        'SELECT SUM(amount) AS totalCredit FROM transactions WHERE customer_id = ? AND type = "credit"',
+        [id],
+        (err, creditResult) => {
+          if (err) {
+            res.status(500).json({ message: 'Error occurred while calculating total credits' });
+          } else {
+            db.query(
+              'SELECT SUM(amount) AS totalDebit FROM transactions WHERE customer_id = ? AND type = "debit"',
+              [id],
+              (err, debitResult) => {
+                if (err) {
+                  res.status(500).json({ message: 'Error occurred while calculating total debits' });
+                } else {
+                  res.status(200).json({
+                    transactions,
+                    totalCredit: creditResult[0].totalCredit || 0,
+                    totalDebit: debitResult[0].totalDebit || 0,
+                  });
+                }
+              }
+            );
+          }
+        }
+      );
     }
   });
 };
