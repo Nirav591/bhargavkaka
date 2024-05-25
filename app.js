b4db06b50ba29282148781011a6046e8da07768f
Nirav591 <nirav.v.lathiya@gmail.com>
2024-05-24 22:06:20 -0700

    added

--- app.js
+++ app.js
@@ -7,6 +7,17 @@ const cors = require('cors');
 const app = express();
 const port = 6350;
 
+
+app.use(
+  cors({
+    origin: 'https://bhargavkaka.libertyworld.in/',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  })
+);
+
 app.use(bodyParser.json());
 app.use(cors({origin:"https://bhargavkaka.libertyworld.in/"}));
 app.use('/api/user', userRoutes);
