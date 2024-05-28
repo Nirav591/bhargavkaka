f692a83bd964eee18f0cb045ce65226608107dc3
Nirav Lathiya <83869023+Nirav591@users.noreply.github.com>
2024-05-28 17:58:18 +0530

    added

--- routes/transactionRoutes.js
+++ routes/transactionRoutes.js
@@ -5,5 +5,6 @@ const transactionController = require('../controllers/transactionController');
 router.post('/add', transactionController.addTransactions);
 router.get('/customer/:id', transactionController.getCustomerTransactions);
 router.put('/update/:id', transactionController.updateTransaction);
+router.delete('/delete/:id', transactionController.deleteTransaction);
 
 module.exports = router;
