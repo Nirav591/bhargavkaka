d54cdc6a39c17b9995dd212b0bc26af9a42435d7
Nirav Lathiya <83869023+Nirav591@users.noreply.github.com>
2024-06-18 16:17:54 +0530

    added

--- routes/customerRoutes.js
+++ routes/customerRoutes.js
@@ -4,12 +4,9 @@ const customerController = require('../controllers/customerController');
 
 router.post('/create', customerController.createCustomer);
 router.post('/add-detail', customerController.addDetail);
-router.get('/:id', customerController.getCustomerDetails);
-router.get('/', customerController.getAllCustomers);
-router.delete('/:id', customerController.deleteCustomer);
-router.delete('/:id', customerController.deleteCustomer);
-router.put('/customer/:id', customerController.updateCustomer);
-
-
+router.get('/:id', customerController.getCustomerDetails); // Get customer details by ID
+router.get('/', customerController.getAllCustomers); // Get all customers
+router.delete('/:id', customerController.deleteCustomer); // Delete customer by ID
+router.put('/customer/:id', customerController.updateCustomer); // Update customer by ID
 
 module.exports = router;
